ba08cb362e7fdc4985856a56aa37c1d70998f2f7
Surfs-Up5 <cgparsons5@gmail.com>
2025-11-01 16:41:58 -0400

    added download buutton not linked to server

--- src/App.js
+++ src/App.js
@@ -12,10 +12,23 @@ function App () {
     const data = await res.json();
     setComic(data);
   }
+
+  async function handleDownload() {
+    if (!comic) {
+      alert("Please load a comic first");
+        return;
+    }
+
+    console.log(`Requesting download for comic #${comic.num}`);
+    alert(`Would request download for comic #${comic.num}`);
+  }
   
   return (
     
     <div style={{ textAlign: "center", padding: 20 }}>
+      <h1>XKCD Comic Viewer</h1>
+
+      <div style={{ marginBottom: 12 }}>
       <input
         type="number"
         placeholder="Comic number (leave blank for latest)"
@@ -23,10 +36,15 @@ function App () {
         onChange={(e) => setComicNum(e.target.value)}
         style={{ marginRight: 8 }}
         />
-      <button onClick={fetchComic}>Load XKCD</button>
+        <button onClick={fetchComic}>Load XKCD</button>
+        <button onClick={handleDownload} style ={{marginLeft: 8}}>
+          Download Comic
+        </button>
+      </div>
+
       {comic && (
         <>
-          <h2>{comic.safe_title}</h2>
+          <h2>{comic.safe_title} - #{comic.num}</h2>
           <img src={comic.img} alt={comic.alt} width="300" />
         </>
       )}
