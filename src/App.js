47a4c3d8c49a0391f2aaf0077f1ba7f3826cf50d
Surfs-Up5 <cgparsons5@gmail.com>
2025-11-01 15:59:34 -0400

    accesses comic on website using Allorigins to bypass CORS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { useState } from "react"
 
 function App () {
-  const[count, setCount] = useState(0);
+  const[comic, setComic] = useState(null);
+  
+  async function fetchComic() {
+    const res = await fetch("https://api.allorigins.win/raw?url=https://xkcd.com/info.0.json");
+    const data = await res.json();
+    setComic(data);
+  }
+  
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+    <div style={{ textAlign: "center", padding: 20 }}>
+      <button onClick={fetchComic}>Load XKCD</button>
+      {comic && (
+        <>
+          <h2>{comic.safe_title}</h2>
+          <img src={comic.img} alt={comic.alt} width="300" />
+        </>
+      )}
     </div>
   );
 }
