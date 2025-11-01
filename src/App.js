fa138e9a85e89efe93ef21f360728e3a6b9fb046
Surfs-Up5 <cgparsons5@gmail.com>
2025-11-01 16:22:19 -0400

    allows user to enter in comic of choice

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import { useState } from "react"
 
 function App () {
   const[comic, setComic] = useState(null);
+  const [comicNum, setComicNum] = useState("");
   
   async function fetchComic() {
-    const res = await fetch("https://api.allorigins.win/raw?url=https://xkcd.com/info.0.json");
+    const url = comicNum
+    ? `https://api.allorigins.win/raw?url=https://xkcd.com/${comicNum}/info.0.json`
+    : "https://api.allorigins.win/raw?url=https://xkcd.com/info.0.json";
+    const res = await fetch(url);
     const data = await res.json();
     setComic(data);
   }
   
   return (
+    
     <div style={{ textAlign: "center", padding: 20 }}>
+      <input
+        type="number"
+        placeholder="Comic number (leave blank for latest)"
+        value={comicNum}
+        onChange={(e) => setComicNum(e.target.value)}
+        style={{ marginRight: 8 }}
+        />
       <button onClick={fetchComic}>Load XKCD</button>
       {comic && (
         <>
